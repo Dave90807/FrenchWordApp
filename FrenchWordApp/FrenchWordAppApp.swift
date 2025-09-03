65bbb940140399247a31fe86ad5ac2dc373dfc8a
DaveC <Dave90807@msn.com>
2025-09-03 14:46:54 -0700

    Initial Commit

---
+++ FrenchWordApp/FrenchWordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchWordAppApp.swift
+//  FrenchWordApp
+//
+//  Created by Dave Cassidy on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FrenchWordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrenchWordAppTests/FrenchWordAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchWordAppTests.swift
+//  FrenchWordAppTests
+//
+//  Created by Dave Cassidy on 9/3/25.
+//
+
+import Testing
+@testable import FrenchWordApp
+
+struct FrenchWordAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
